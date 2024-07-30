a8c385b5a8bbcd56f06f537a6e76cc1961b1873a
Kalyan Kumar <kalyaniit795@gmail.com>
2024-07-31 02:55:47 +0530

    text error resolve

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faPenToSquare, faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 import todoImg from './Image/to-do-list.png'
+import { FaGithub } from "react-icons/fa";
 
 const getLocalItems = () => {
   let list = localStorage.getItem('lists');
@@ -75,14 +76,14 @@ function App() {
 
   return (
     <>
-      <div className='bg-slate-900 w-full min-h-screen text-white flex flex-col items-center p-8'>
+      <div className='bg-slate-900 w-full h-[96.5vh] text-white flex flex-col items-center p-8'>
         <h2 className='text-4xl mb-3 font-medium font-pacifico flex items-center'><img src={todoImg} alt='error' className='w-16'></img>Todo List</h2>
         <div className='flex items-center gap-2 font-medium p-2 flex-col'>
           {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
           {
             editMode ? (
-              <div className='flex gap-2 w-full'>
-                <input type='text' value={editValue} onChange={(e) => setEditValue(e.target.value)} onKeyPress={(e) => handleKeyPress(e, updateTodo)} className='rounded-sm p-2 text-black hover:border-green-400 border-2 outline-none '></input>
+              <div className='flex gap-2'>
+                <input type='text' value={editValue} onChange={(e) => setEditValue(e.target.value)} onKeyPress={(e) => handleKeyPress(e, updateTodo)} className='rounded-sm p-2 text-black hover:border-green-400 border-2 outline-none sm:w-80'></input>
                 <button onClick={updateTodo} className='bg-red-700 p-1 rounded-md hover:bg-green-400 hover:text-black px-3'><FontAwesomeIcon icon={faPenToSquare} className='w-8 h-7' /></button>
               </div>
             ) :
@@ -95,16 +96,18 @@ function App() {
           }
         </div>
         <div className='w-90 flex justify-center items-center sm:w-full mt-4'>
-          <ul className='flex flex-col justify-around p-1 gap-2 items-center max-h-80 overflow-x-hidden overflow-y-scroll no-scrollbar'>
+          <ul className='flex flex-col justify-around p-1 gap-1 items-center max-h-80 overflow-x-hidden overflow-y-scroll no-scrollbar'>
             {
               todos.map((todo, index) => (
-                <li key={index} className='px-1 rounded-sm flex w-80 items-center justify-center sm:w-[30rem] md:w-[35rem]'>
+                <li key={index} className='p-3 rounded-sm flex w-80  gap-1 items-center justify-center sm:w-[30rem] md:w-[35rem]'>
                   <img src={todoImg} className='w-10'></img>
-                  <div className='bg-blue-950 flex p-3 sm:min-w-96 md:w-[30rem] hover:border-2 border-white items-center justify-between'>
-                    <p className='w-48 truncate flex'> {todo.text}</p>
+                  <div className='  sm:min-w-96 md:w-[30rem] w-64'>
+                  <div className='bg-blue-950 flex p-2  hover:bg-slate-700 items-center justify-between h-12 '>
+                    <p className='w-36 sm:w-48 capitalize truncate '> {todo.text}</p>
                     <div className='gap-5 flex'>
-                      <button onClick={() => deleteTodo(todo.id)} className='text-red-600'><FontAwesomeIcon icon={faTrash} /></button>
-                      <button onClick={() => editTodo(todo.id, todo.text)} className='text-green-500'><FontAwesomeIcon icon={faPenToSquare} /></button>
+                      <button onClick={() => deleteTodo(todo.id)} className='hover:text-red-600'><FontAwesomeIcon icon={faTrash} /></button>
+                      <button onClick={() => editTodo(todo.id, todo.text)} className='hover:text-green-600'><FontAwesomeIcon icon={faPenToSquare} /></button>
+                    </div>
                     </div>
                   </div>
                 </li>
@@ -112,8 +115,9 @@ function App() {
             }
           </ul>
         </div>
-        <p className='bg-slate-600 w-full text-center py-0 absolute bottom-0 '>Copyright @Kalyanam</p>
+        
       </div>
+      <p className='bg-slate-500 text-white w-full flex text-center py-0 bottom-0 p-1 gap-1 items-center justify-center'>Copyright  <a href='https://github.com/Kalyan190' className='' target='_blank'><FaGithub className='text-white bg-black rounded-full cursor-pointer'  /></a> @Kalyanam</p>
       
     </>
   )
